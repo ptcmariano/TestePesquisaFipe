9588e8223adbdec907af169bb9a92ea6467c39ed
Paulo Tiago <paulotiagocm@hotmail.com>
2013-09-16 08:25:57 -0300

    Update ConsultaPrecoMedioVeiculos.java

--- src/br/org/fipe/ConsultaPrecoMedioVeiculos.java
+++ src/br/org/fipe/ConsultaPrecoMedioVeiculos.java
@@ -14,40 +14,36 @@ public class ConsultaPrecoMedioVeiculos extends ObjetoPaginaBase {
 	 * Navegar para a pagina de consulta de carros e utilitarios
 	 */
 	public void consultaCarrosClickLink(){
-		//funcao.navegar("http://www.fipe.org.br/web/indices/veiculos/default.aspx?p=51");
 		funcao.usarFrame().framePorID("fconteudo");
 		funcao.clicar(EnumElemento.XPATH, "//*[@id='lblTexto']/p[2]/a[1]");
 	}
 	/**
 	 * Selecionar uma marca de carro a partir do texto da marca 
-	 * @param valor Passar texto que é exibido da marca
+	 * @param valor Passar texto que ï¿½ exibido da marca
 	 */
 	public void selecionaMarca(String marca){
 		funcao.usarLista(EnumElemento.ID, "ddlMarca").selecionarOpcaoPeloTexto(marca);
 	}
 	/**
 	 * Selecionar o modelo a partir do texto
-	 * @param modelo Passar texto que é exibido do modelo
+	 * @param modelo Passar texto que ï¿½ exibido do modelo
 	 */
 	public void selecionarModelo(String modelo){
-		//funcao.usarEspera().atributoPresente(funcao.pegarElemento(EnumElemento.ID, "ddlModelo"), "disabled", "false", 10000);
 		funcao.usarEspera().elementoEstarClicavel(EnumElemento.ID, "ddlModelo", 10);
 		funcao.usarLista(EnumElemento.ID, "ddlModelo").selecionarOpcaoPeloTexto(modelo);
 	}
 	/**
 	 * Selecionar o ano
-	 * @param passar texto que é exibido na lista
+	 * @param passar texto que ï¿½ exibido na lista
 	 */
 	public void selecionarAno(String ano){
-		//     \/ não é a melhor forma! mas as vezes quebra sem aguardar =(
-		//funcao.aguardar(2000);
 		//
 		funcao.usarEspera().elementoEstarClicavel(EnumElemento.ID, "ddlAnoValor", 10);
 		funcao.usarLista(EnumElemento.ID, "ddlAnoValor").selecionarOpcaoPeloTexto(ano);
 	}
 	/**
-	 * Retorna o valor exibido da consulta de preços medios de veiculos<br />
-	 * Utilize sempre após selecionar o ANO para evitar erros :x
+	 * Retorna o valor exibido da consulta de preï¿½os medios de veiculos<br />
+	 * Utilize sempre apï¿½s selecionar o ANO para evitar erros :x
 	 * @return retorna valor exibido na pesquisa
 	 */
 	public float buscaValorResultado(){
